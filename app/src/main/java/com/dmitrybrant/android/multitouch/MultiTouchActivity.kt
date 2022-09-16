4386df489d0c05e95e2c0c237054c5697f159c56
Dmitry Brant <me@dmitrybrant.com>
2022-09-16 11:59:35 -0400

    Simplify a bit.

--- app/src/main/java/com/dmitrybrant/android/multitouch/MultiTouchActivity.kt
+++ app/src/main/java/com/dmitrybrant/android/multitouch/MultiTouchActivity.kt
@@ -32,10 +32,10 @@ class MultiTouchActivity : Activity(), MultiTouchStatusListener {
     }
 
     override fun onStatus(pointerLocations: List<Point>, numPoints: Int) {
-        val str = StringBuilder(String.format(resources.getString(R.string.num_touches), numPoints.toString()))
+        val str = StringBuilder(String.format(getString(R.string.num_touches), numPoints))
         for (i in 0 until numPoints) {
             str.append("\n")
-            str.append(pointerLocations[i].x.toString())
+            str.append(pointerLocations[i].x)
             str.append(", ")
             str.append(pointerLocations[i].y)
         }
